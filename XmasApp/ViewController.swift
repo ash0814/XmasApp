17f1e06d949eb22896058d474c5be08f6328e333
Sehyeon An <ash@Sehyeonui-MacBookAir.local>
2021-12-20 17:52:11 +0900

    add: openLibrary, openCamera

--- XmasApp/ViewController.swift
+++ XmasApp/ViewController.swift
@@ -11,18 +11,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var Nav: UINavigationBar!
     @IBOutlet weak var imageView: UIImageView!
     
+    let picker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.delegate = self
         // Do any additional setup after loading the view.
         imageView.image = UIImage(named: "profile")
     }
+    
+   
+    
     @IBAction func addAct(_ sender: Any) {
         let alert =  UIAlertController(title: "타이틀", message: "메세지", preferredStyle: .actionSheet)
-        
+        let library =  UIAlertAction(title: "사진앨범", style: .default) { (action) in self.openLibrary()
+        }
+
+        let camera =  UIAlertAction(title: "카메라", style: .default) { (action) in
+        self.openCamera()
+        }
         let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(library)
+        alert.addAction(camera)
         alert.addAction(cancel)
         present(alert, animated: true, completion: nil)
     }
+    
+    func openLibrary(){
+      picker.sourceType = .photoLibrary
+      present(picker, animated: false, completion: nil)
+    }
+    func openCamera(){
+      picker.sourceType = .camera
+      present(picker, animated: false, completion: nil)
+    }
+   
 }
 
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+}
